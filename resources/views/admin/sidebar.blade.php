54c0e30a7fa2d0393286ec8cd181e3b7e32a1aea
Onur Göğebakan <62071248+Onurgbkn@users.noreply.github.com>
2021-08-21 17:30:05 +0300

    s

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -9,7 +9,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{asset('adminpanel')}}/index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{route('dashboard')}}">
                 <div class="sidebar-brand-text mx-3">Admin</div>
             </a>
 
